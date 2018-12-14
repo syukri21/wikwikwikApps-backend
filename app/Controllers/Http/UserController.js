ac7e7f55bf92d01db4cf1e46516a36c474f8ff41
syukri21 <syukrihsb148@gmail.com>
2018-12-14 14:12:09 +0700

    add user comtroller

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -4,7 +4,11 @@ const Database = use('Database');
 
 class UserController {
 	async getAllScore({ request, response }) {
-		const results = await Database.select('username', 'score').from('users');
+		const results = await Database.select('username', 'score')
+			.from('users')
+			.orderBy('score', 'desc')
+			.limit(5);
+
 		response.send({
 			messages : 'success',
 			results
